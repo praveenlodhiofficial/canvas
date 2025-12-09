0278c1422eba92546058656267523361f1dd7834
Praveen Lodhi <praveenlodhi.official@gmail.com>
2025-12-09 15:41:04 +0000

    feat: intialize bun native server for http & websocket

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun --- http-backend!");
+const server = Bun.serve({
+  port: 3001,
+  fetch(request) {
+    return new Response("Welcome to Bun http server")
+  }
+})
+
+console.log(`HTTP server running on ${server.url}`);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3002,
+
+  fetch(req, server) {
+    // If request is asking for WebSocket → upgrade connection
+    if (server.upgrade(req)) return;
+
+    return new Response("WS Backend Running!", { status: 200 });
+  },
+
+  websocket: {
+    open(ws) {
+      console.log("Client connected");
+      ws.send(JSON.stringify({ type: "welcome", message: "Connected to WS server" }));
+    },
+
+    message(ws, message) {
+      console.log("Client says:", message);
+
+      // Echo message back
+      ws.send(JSON.stringify({ type: "echo", message }));
+    },
+
+    close(ws) {
+      console.log("Client disconnected");
+    }
+  }
+});
+
+console.log(`WebSocket server running on ${server.url}`);
